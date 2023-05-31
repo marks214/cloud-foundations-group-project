6c552540289ca0967683dd6b2fd7d0171074b10c
marks214 <aimee.oz.x@gmail.com>
2023-05-31 18:51:04 +0000

    created buildspec-c.yml for codebuild in console, added codebuild resource block to main.tf

--- main.tf
+++ main.tf
@@ -206,3 +206,37 @@ resource "aws_iam_role_policy_attachment" "codebuild_role_policy" {
   role = aws_iam_role.codebuild_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
+
+# -------------------------CODEBUILD-------------------------------
+
+resource "aws_codebuild_project" "codebuild_project" {
+  name           = "cohort4-group3-cap2"
+  description    = "cohort4-group3-cap2 codebuild_project"
+  build_timeout  = "60"
+  queued_timeout = "480"
+
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type  = "NO_CACHE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode = true
+
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/marks214/cloud-foundations-group-project.git"
+  }
+
+}
