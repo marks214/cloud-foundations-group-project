3e961c6b866ade1774653bafdd691d6366afdcf3
marks214 <aimee.oz.x@gmail.com>
2023-06-01 21:54:20 +0000

    test

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ data "aws_s3_bucket" "tf_state_bucket" {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-west-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  #access_key = var.access_key
+  #secret_key = var.secret_key
 }
 
 resource "aws_s3_bucket" "json_bucket" {
--- var.tf
+++ var.tf
@@ -1,7 +1,11 @@
 variable "access_key" {
   type = string
+  default = ""
+  sensitive = true
 }
 
 variable "secret_key" {
   type = string
+  default = ""
+  sensitive = true
 }
